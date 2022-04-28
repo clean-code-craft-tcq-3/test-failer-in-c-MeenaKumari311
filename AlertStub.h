0e6e0c8aa899bbe9e132f5fdf9b54faba42efb3b
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-29 00:21:57 +0530

    Update AlertStub.h

--- AlertStub.h
+++ AlertStub.h
@@ -1,4 +1,17 @@
 #ifndef __ALERTSTUB_H_
 #define __ALERTSTUB_H_
-int networkAlertStub(float celcius);
+int networkAlertStub(float celcius) {
+    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+    // Return 200 for ok
+    // Return 500 for not-ok
+    // stub always succeeds and returns 200
+  if(celcius>100)
+  {
+    return 500;
+  }
+  else
+  {
+    return 200;
+  }
+}
 #endif
