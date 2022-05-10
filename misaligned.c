a1cb31bb49983da6c462d26b951ffb9037818c9b
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-05-11 04:59:30 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -7,9 +7,9 @@ int printColorMap() {
     int i = 0, j = 0, pairNo=0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            printf("%d | %s | %s\n", i * 5 + j+1, majorColor[i], minorColor[j]);
             pairNo++;
-            assert((i * 5 + j)==pairNo);
+            assert((i * 5 + j+1)==pairNo);
         }       
     }
     return i * j;
